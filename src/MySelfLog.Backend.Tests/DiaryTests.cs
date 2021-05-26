2400164f4684d46e170eed623f610728a83acfcb
Riccardo Di Nuzzo <riccardo@dinuzzo.it>
2021-05-26 12:14:58 +0100

    Unit tests (#24)
    * dockerfile; nlog fix
    
    * unit tests
    
    * cleanup

---
+++ src/MySelfLog.Backend.Tests/DiaryTests.cs
@@ -0,0 +1,25 @@
+using MySelfLog.Backend.Domain;
+using MySelfLog.Backend.Domain.Aggregates;
+using MySelfLog.Backend.Domain.Commands;
+using NUnit.Framework;
+
+namespace MySelfLog.Backend.Tests
+{
+    public class Tests
+    {
+        [Test]
+        public void Given_Valid_CreateDiary_I_Expect_The_Diary_Created()
+        {
+            // Assign
+            var request = Helpers.BuilCloudRequest(".\\PayloadSamples\\creatediary.json");
+            
+            // Act
+            var mapper = new CreateDiaryMapper();
+            var cmd = mapper.Map(request) as CreateDiary;
+            var result = Diary.Create(cmd);
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
